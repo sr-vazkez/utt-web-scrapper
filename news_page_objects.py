bd50f10f04f06546a99ededcf37b101f63af621c
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2020-09-21 15:22:22 -0500

    mas codigo

--- news_page_objects.py
+++ news_page_objects.py
@@ -4,22 +4,12 @@ import requests
 
 from common import config
 
-class HomePage:
+class HomePage(NewsPage):
 
     def __init__(self, news_site_uid, url):
-        self._config = config()['news_sites'][news_site_uid]
-        self._queries = self._config['queries']
-        self._html = None
-
-        self._visit(url)
-
-    def _visit(self, url):
-        response = requests.get(url)
-        response.raise_for_status()
-        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+        super().__init__(news_site_uid, url)
 
     @property
-
     def article_links(self):
         link_list = []
         for link in self._select(self._queries['homepage_article_links']):
@@ -29,3 +19,16 @@ class HomePage:
     
     def _select(self, query_string):
         return self._html.select(query_string)
+
+class ArticlePage(NewsPage):
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid)
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
