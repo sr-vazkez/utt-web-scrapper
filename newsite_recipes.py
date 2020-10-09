21ed19737bf7463dca1fde07f177dca2de453b1b
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2020-10-08 21:48:17 -0500

    haciendo funciones

---
+++ newsite_recipes.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+import urllib.parse import urlparse
+
+import pandas as pd
+
+logger = loggin.getLogger(__name__)
+
+def main(filename):
+    logger.info('Empezando el proseso de limpiado')
+
+    df = _read_data(filename)
+    newspaper_uid = _extract_newspaper_uid(filename)
+    df = _add_newspaper_uid_column(df, newspaper_uid) 
+    df = _extract_host(df)
+
+def _read_data(filename):
+    logger.info('Leyendo el archivo {}'.format(filename))
+    return pd.read_csv(filename)
+
+def _extract_newspaper_uid(filename):
+    logger.info('Extrayendo el newspaper uid')
+    newspaper_uid = filename.split('_')[0]
+
+    logger.info('Newspaper uid detectado: {}'.format(newspaper_uid))
+    return newspaper_uid 
+
+def _add_newspaper_uid_column(df, newspaper_uid):
+    logger.info('LLenando la columna newspaper_uid con {}'.format(newspaper_uid))
+    df['newspaper_uid'] = newspaper_uid
+
+    return df
+
+def _extract_host(df):
+    logger.info('Extrayendo el host de las urls')
+    df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',help='La raiz de los datos sucios', type=str)
+
+    arg = parser.parse_args()
+
+    main(args.filename) 
